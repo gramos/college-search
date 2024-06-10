65563442e56818c0fa64e3df8ce47ebc8192841e
Gaston Ramos <ramos.gaston@gmail.com>
2024-06-10 19:03:09 -0300

    Use simple css stylesheet

--- lib/college_api.rb
+++ lib/college_api.rb
@@ -2,7 +2,8 @@ class CollegeApi
 
   def self.search(name)
     fields  = "_fields=school.name,location.lat,location.lon" 
-    uri     = URI("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=#{key}&#{fields}&school.name=#{name}")
+    uri     = URI("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=#{self.key}&#{fields}&school.name=#{name}")
+    Rails.logger.warn("URI: #{uri}")
     results = Net::HTTP.get(uri)
 
     JSON.parse(results)
