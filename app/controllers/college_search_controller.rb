fecee09ace7a40879a61c0ff1a7efe23778b16b8
Gaston Ramos <ramos.gaston@gmail.com>
2024-06-10 16:54:42 -0300

    Center maps after search working properly

--- app/controllers/college_search_controller.rb
+++ app/controllers/college_search_controller.rb
@@ -3,17 +3,17 @@ require 'net/http'
 class CollegeSearchController < ApplicationController
 
   def index
-    Rails.logger.warn("EN INDEX -----------------------------")
-    @aux = "AUX"
+    @center = { 'lat' => 0, 'lon' => 0 }
   end
 
   def search
     api_key = "qo1hY1KnV9gWzslbA3y2nK9fLe2ZXtRKfOVndNha"
-    uri     = URI("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=#{api_key}")
+    fields  = "_fields=school.name,location.lat,location.lon" 
+    uri     = URI("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=#{api_key}&#{fields}&school.name=#{params['college_name']}")
 
-    @results = Net::HTTP.get(uri)
+    @results  = Net::HTTP.get(uri)
     @colleges = JSON.parse(@results)
-    Rails.logger.warn(@colleges.inspect)
+    @center   = Geo.calculate_center(@colleges['results'])
 
     render :index
   end
---
+++ lib/geo.rb
@@ -0,0 +1,49 @@
+class Geo
+ # Necesitamos la biblioteca Math para varias operaciones matemáticas
+  include Math
+
+  def self.to_radians(degrees)
+   degrees * PI / 180.0
+  end
+
+  def self.to_degrees(radians)
+    radians * 180.0 / PI
+  end
+
+  def self.remove_nil(points)
+    points.map{|p| p unless p['location.lat'].nil? or p['location.lon'].nil? }.compact
+  end
+  
+  def self.calculate_center(points)
+    points = remove_nil(points)
+
+    Rails.logger.warn('GEO ------------------------------ ')
+    Rails.logger.warn(points.inspect)
+    
+    # Convert to radians
+    
+    lat_rad = points.map { |point| to_radians(point['location.lat']) }
+    lon_rad = points.map { |point| to_radians(point['location.lon']) }
+
+    # Convertir a coordenadas cartesianas
+    x = lat_rad.zip(lon_rad).map { |lat, lon| Math.cos(lat) * Math.cos(lon) }
+    y = lat_rad.zip(lon_rad).map { |lat, lon| Math.cos(lat) * Math.sin(lon) }
+    z = lat_rad.map { |lat| Math.sin(lat) }
+
+    # Calcular el punto medio en coordenadas cartesianas
+    x_avg = x.sum / x.size
+    y_avg = y.sum / y.size
+    z_avg = z.sum / z.size
+
+    # Convertir de nuevo a coordenadas esféricas
+    lon_avg = Math.atan2(y_avg, x_avg)
+    hyp     = Math.sqrt(x_avg**2 + y_avg**2)
+    lat_avg = Math.atan2(z_avg, hyp)
+
+    # Convertir a grados
+    lat_center = to_degrees(lat_avg)
+    lon_center = to_degrees(lon_avg)
+    
+    { 'lat' => lat_center, 'lon' => lon_center }
+  end
+end
