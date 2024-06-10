2b170b78f73e6afae59b6d59ee2b7c12984845e9
Gaston Ramos <ramos.gaston@gmail.com>
2024-06-10 15:57:12 -0300

    First working version show gmaps with markers.

--- app/controllers/college_search_controller.rb
+++ app/controllers/college_search_controller.rb
@@ -1,5 +1,20 @@
+require 'net/http'
+
 class CollegeSearchController < ApplicationController
 
   def index
+    Rails.logger.warn("EN INDEX -----------------------------")
+    @aux = "AUX"
+  end
+
+  def search
+    api_key = "qo1hY1KnV9gWzslbA3y2nK9fLe2ZXtRKfOVndNha"
+    uri     = URI("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=#{api_key}")
+
+    @results = Net::HTTP.get(uri)
+    @colleges = JSON.parse(@results)
+    Rails.logger.warn(@colleges.inspect)
+
+    render :index
   end
 end
