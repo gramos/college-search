82c63e9bcbef9fe92678d9b258fb9b2fac2ecc2e
Gaston Ramos <ramos.gaston@gmail.com>
2024-06-11 15:10:52 -0300

    Fix google api key

--- lib/college_api.rb
+++ lib/college_api.rb
@@ -2,7 +2,7 @@ class CollegeApi
 
   def self.search(name)
     fields  = "_fields=school.name,location.lat,location.lon" 
-    uri     = URI("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=#{self.key}&#{fields}&school.name=#{name}")
+    uri     = URI("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=#{self.key}&#{fields}&school.name=#{name}&per_page=100")
 
     results =  Rails.cache.fetch("#{name}/search", expires_in: 2.days) do
        Rails.logger.warn("<<< Retrieving Data from: api.data.gov >>>")
